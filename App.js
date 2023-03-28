f42debed4180ff62c2d89cb7f11ecde532de9a27
shubham <sm863037@gmail.com>
2023-03-29 00:04:59 +0530

    How do I create nested element

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 //When we did create element, Its basically create an object
-const heading = React.createElement(
-  'h1',
-  { id: 'heading', xyz: 'xyz' },
-  'Hello world! from react'
+//how do I create nested element?
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement(
+    'div',
+    { id: 'child' },
+    React.createElement('h1', {}, 'I am hi tag')
+  )
 )
-console.log(heading) // This is an object
+
+console.log(parent) // This is an object and this object is react element
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 //This render methode will basically be responsible to take this object and put it up
-//converted into heading (h1) tag and put it up on the DOM(In the <div></div>)
+//converted into html (heading (h1) tag )and put it up on the DOM(In the <div></div>)
 
-root.render(heading)
+root.render(parent)
--- README.md
+++ README.md
@@ -62,3 +62,24 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(heading)
 ```
+//how do I create nested element?
+```js
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement(
+    'div',
+    { id: 'child' },
+    React.createElement('h1', {}, 'I am hi tag')
+  )
+)
+
+console.log(parent) // This is an object and this object is react element
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+//This render methode will basically be responsible to take this object and put it up
+//converted into html (heading (h1) tag )and put it up on the DOM(In the <div></div>)
+
+root.render(parent)
+```
