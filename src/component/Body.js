c8ec89d6fe3c59aabc0559a1108b521d8dc1b790
shubham <sm863037@gmail.com>
2023-04-14 13:45:40 +0530

    lecture_5

--- src/component/Body.js
+++ src/component/Body.js
@@ -3,13 +3,9 @@ import resList from "../utils/mockData"
 import { useState } from "react"
 
 const Body = () => {
-  const datal =  useState(resList)
-  console.log(datal)
-  const list = datal[0]
-  const setList = datal[1]
-  console.log(setList)
-
-    const filterData = () => {
+  const [list, setList] =  useState(resList)
+  
+  const filterData = () => {
         const updateList =  resList.filter((rate => {
             return rate.data.avgRating >= 4.1
         }))
